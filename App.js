b841c369cec9a958c334fa4e1ef176a11dc01718
JoaoVitorErmacora <joao.caramello@gmail.com>
2025-08-13 10:08:16 -0300

    commit

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Ofgpen up uApp.js to start working on your app!</Text>
+      <Text>Ofgpenewqewq up uApp.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
