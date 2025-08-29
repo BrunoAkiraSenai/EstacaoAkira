147132754bf25320e80786c127ce4f48b9838287
BrunoAkiraSenai <akirafurumori@gmail.com>
2025-08-29 09:53:14 -0300

    erro

--- App.js
+++ App.js
@@ -1,25 +1,23 @@
-import { StatusBar } from "expo-status-bar";
-import { ImageBackground, StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import Onboarding from "./src/pages/Onboarding";
+import SignUp from "./src/pages/SignUp";
+import SignIn from "./src/pages/SignIn";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <ImageBackground
-      source={require("./src/imagem/27calor.jpg")}
-      style={styles.container}
-    >
-      <View>
-        <Text>Openg on your app!</Text>
-        <StatusBar style="auto" />
-      </View>
-    </ImageBackground>
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Onboarding"
+        screenOptions={{ headerShown: false }}
+      >
+        <Stack.Screen name="Onboarding" component={Onboarding} />
+        <Stack.Screen name="SignUp" component={SignUp} />
+        <Stack.Screen name="SignIn" component={SignIn} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
