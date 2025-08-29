a43b8826b0b81e6e8077704d4307ec1270b5a492
JoaoVitorErmacora <joao.caramello@gmail.com>
2025-08-29 14:42:16 -0300

    cradastro

--- src/pages/cadastro.js
+++ src/pages/cadastro.js
@@ -10,10 +10,13 @@ import {
 import { StatusBar } from "expo-status-bar";
 
 // Import Styles!
+import { stylesSign } from "../styles/StylesSign";
 
+import { StylesOnboarding } from "../styles/StylesOnboarding";
 
 // Import Icon!
 import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
+import InputComp from "../components/InputComp";
 
 import { useNavigation } from "@react-navigation/native";
 
@@ -21,7 +24,8 @@ export default function SignUp() {
   const navigation = useNavigation();
   return (
     <ImageBackground
-      source={require("./src/images/download.jpg")}
+      style={stylesSign.container}
+      source={require("../images/wallpaper.jpg")}
       blurRadius={15}
     >
       <View
@@ -39,12 +43,14 @@ export default function SignUp() {
           color="white"
           style={{ marginBottom: 20 }}
         />
-        <Text >
+        <Text style={stylesSign.title}>Sign Up</Text>
+        <Text style={stylesSign.text}>
           Cadastre-se na Estação Akira!
         </Text>
 
         <View style={{ marginTop: 80 }}>
           <TextInput
+            style={stylesSign.input}
             placeholderTextColor={"#bebebe"}
             placeholder="Name"
           />
@@ -55,12 +61,13 @@ export default function SignUp() {
         </View>
 
         <TouchableOpacity
+          style={[StylesOnboarding.btn, { marginTop: 80, width: "100%" }]}
         >
-          <Text >Sign Up</Text>
+          <Text style={StylesOnboarding.txt2}>Sign Up</Text>
         </TouchableOpacity>
 
         <View style={StylesOnboarding.viewSignUp}>
-          <Text >Já tem conta?</Text>
+          <Text style={StylesOnboarding.txt2}>Já tem conta?</Text>
           <Pressable onPress={() => navigation.navigate("SignUp")}>
             <Text style={{ fontWeight: "bold", color: "#fff" }}>Sign In</Text>
           </Pressable>
