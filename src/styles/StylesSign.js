c5a915b624010b9e66d031998fa0c4e1d7e215be
RodrigoSilva52 <elenarosangela15@gmail.com>
2025-09-05 15:17:54 -0300

    Merge finalizado

--- src/styles/StylesSign.js
+++ src/styles/StylesSign.js
@@ -22,9 +22,6 @@ export const stylesSign = StyleSheet.create({
     borderBottomWidth: 1,
     borderBottomColor: "#bebebe",
     paddingLeft: 7,
-<<<<<<< HEAD
     color: "#bebebe",
-=======
->>>>>>> master
   },
 });
