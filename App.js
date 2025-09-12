09e6d97a252ceb490600470526830d71156b8e6a
RodrigoSilva52 <elenarosangela15@gmail.com>
2025-09-12 08:09:36 -0300

    arrumado o app.js

--- App.js
+++ App.js
@@ -1,27 +1,12 @@
-<<<<<<< HEAD
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, ImageBackground } from "react-native";
-
-import SignIn from "./src/pages/SingIn";
-// import SignUp from "./src/pages/SingUp";
-=======
 import { NavigationContainer } from "@react-navigation/native";
->>>>>>> origin/DavidAndrade
 
-import { NavigationContainer } from "@react-navigation/native"; // corrigido aqui
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-<<<<<<< HEAD
-import { useState } from "react";
-
-const Stack = createNativeStackNavigator(); // adicionado
-=======
 import Onboarding from "./src/pages/Onboarding";
 import SignUp from "./src/pages/SignUp";
 import SignIn from "./src/pages/SignIn";
 
 const Stack = createNativeStackNavigator();
->>>>>>> origin/DavidAndrade
 
 export default function App() {
   0;
@@ -38,12 +23,3 @@ export default function App() {
     </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
